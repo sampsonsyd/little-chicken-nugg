1cc0950e3b01ca671493cf99803615ca81db55fe
sampsonsyd <91164160+sampsonsyd@users.noreply.github.com>
2021-10-26 20:45:40 -0400

    2

--- exercises/intro_to_javascript/script.js
+++ exercises/intro_to_javascript/script.js
@@ -1,1 +1,16 @@
-function sayHello(){}
+let age = 25;
+let height = 5;
+
+console.log(age);
+console.log(height);
+
+
+let noun = `cat`;
+let verb= `bounds`;
+let adjective= `see`;
+
+console.log(noun);
+console.log(verb);
+console.log(adjective);
+
+'My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky;'
