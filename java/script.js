5c2dd3ff82fb5b783bfdc5c41ea20228fbecb37d
sampsonsyd <91164160+sampsonsyd@users.noreply.github.com>
2021-11-09 20:54:46 -0500

    1

--- java/script.js
+++ java/script.js
@@ -11,6 +11,9 @@ if( age <8){
 
 
 //let item = arryName[Math.floor(Math.random())*arryName.lenght)]
+let noun = nouns[Math.floor(0)]
+
+
 let array = ["1"]
     array[2]
     console.log('It works');
@@ -19,4 +22,17 @@ let array = ["1"]
     let verbs = ["look", "make", "continue"];
     let adjectives =["good", "different", "possible"];
 
-    let noun = 
+    let noun = nouns[random]
+    let verbs = verbs[random]
+
+    console.log(nouns, random)
+    console.log(verbs, random])
+
+    let sentance = 'My ${noun} leaps ${adjectives} when I ${verb} a rainbow in the sky.'
+
+    let ranks = [ 'a', 'b', 'c',];
+    for (let i= 0; i < ranks.length)
+
+    for(ranks of ranks){
+        console.log(rank);
+    }
