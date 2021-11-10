d1cea21c79b96be9f752993caba6685a3287aafe
sampsonsyd <91164160+sampsonsyd@users.noreply.github.com>
2021-11-09 20:30:02 -0500

    1

---
+++ java/script.js
@@ -0,0 +1,22 @@
+//exercise 06
+let age= 12;
+let height= 5;
+if( age <8){
+    console.log("Check out the Merry Go Round. You'll love it");
+}else if ( age> 8 && <65){
+    console.log("Check out the Merry Go Round. You'll love it");
+}else{
+    console.log(some )
+}
+
+
+//let item = arryName[Math.floor(Math.random())*arryName.lenght)]
+let array = ["1"]
+    array[2]
+    console.log('It works');
+
+    let nouns =["heart", "rainbow", "ocean"];
+    let verbs = ["look", "make", "continue"];
+    let adjectives =["good", "different", "possible"];
+
+    let noun = 
