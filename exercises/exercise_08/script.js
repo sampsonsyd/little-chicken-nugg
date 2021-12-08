aecb5c5bcb6837f9cbdd0f32ddf0a9395eda0390
sampsonsyd <91164160+sampsonsyd@users.noreply.github.com>
2021-12-08 10:25:58 -0500

    Add files via upload

---
+++ exercises/exercise_08/script.js
@@ -0,0 +1,38 @@
+//exercise 06
+let age= 12;
+let height= 5;
+if( age <8){
+    console.log("Check out the Merry Go Round. You'll love it");
+}else if ( age> 8 && <65){
+    console.log("Check out the Merry Go Round. You'll love it");
+}else{
+    console.log(some )
+}
+
+
+//let item = arryName[Math.floor(Math.random())*arryName.lenght)]
+let noun = nouns[Math.floor(0)]
+
+
+let array = ["1"]
+    array[2]
+    console.log('It works');
+
+    let nouns =["heart", "rainbow", "ocean"];
+    let verbs = ["look", "make", "continue"];
+    let adjectives =["good", "different", "possible"];
+
+    let noun = nouns[random]
+    let verbs = verbs[random]
+
+    console.log(nouns, random)
+    console.log(verbs, random])
+
+    let sentance = 'My ${noun} leaps ${adjectives} when I ${verb} a rainbow in the sky.'
+
+    let ranks = [ 'a', 'b', 'c',];
+    for (let i= 0; i < ranks.length)
+
+    for(ranks of ranks){
+        console.log(rank);
+    }
