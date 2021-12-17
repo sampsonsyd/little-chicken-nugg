ce55dec42b4c8e07a2a8d4b1fee531ffc8245044
sampsonsyd <91164160+sampsonsyd@users.noreply.github.com>
2021-12-17 01:04:21 -0500

    4

--- exercises/exercise_08/script.js
+++ exercises/exercise_08/script.js
@@ -1,36 +1,28 @@
+ /*Adding a class when the toggle box is clicked*/
+ let menutoggle = document.querySelector('.toggle');
+ menutoggle.onclick = function(){
+     menutoggle.classList.toggle('active')
+ }
+
 // Select each button
-var grayBtn = document.querySelector('#grayButton');
+var blackBtn = document.querySelector('#blackButton');
 var whiteBtn = document.querySelector('#whiteButton');
-var blueBtn = document.querySelector('#blueButton');
-var yellowBtn = document.querySelector('#yellowButton');
+
 
 // Add an event listener
-grayBtn.onclick = switchGray;
+blackBtn.onclick = switchBlack;
 whiteBtn.onclick = switchWhite;
-blueBtn.onclick = switchBlue;
-yellowBtn.onclick = switchYellow;
+
 
 // Write the event handler
-function switchGray() {
-  var body = document.querySelector('body');
-  body.style.backgroundColor = '#815E5B'; 
-  body.style.color = 'white'; 
+function switchBlack() {
+var body = document.querySelector('body');
+body.style.backgroundColor = '#000000'; 
+body.style.color = 'white'; 
 }
 
 function switchWhite() {
-  var body = document.querySelector('body');
-  body.style.backgroundColor = '#7A6F9B'; 
-  body.style.color = 'white'; 
-}
-
-function switchBlue() {
-  var body = document.querySelector('body');
-  body.style.backgroundColor = '#8B85C1'; 
-  body.style.color = 'white'; 
-}
-
-function switchYellow() {
-  var body = document.querySelector('body');
-  body.style.backgroundColor = '#D4CDF4'; 
-  body.style.color = 'white'; 
+var body = document.querySelector('body');
+body.style.backgroundColor = '#faf9f7'; 
+body.style.color = 'black'; 
 }
