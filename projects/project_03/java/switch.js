060e894d5f98d68960018a7f01abed44b6e42141
sampsonsyd <91164160+sampsonsyd@users.noreply.github.com>
2021-12-17 23:54:19 -0500

    d

---
+++ projects/project_03/java/switch.js
@@ -0,0 +1,22 @@
+// Select each button
+var blackBtn = document.querySelector('#blackButton');
+var whiteBtn = document.querySelector('#whiteButton');
+
+
+// Add an event listener
+blackBtn.onclick = switchBlack;
+whiteBtn.onclick = switchWhite;
+
+
+// Write the event handler
+function switchBlack() {
+  var body = document.querySelector('body');
+  body.style.backgroundColor = '#000000'; 
+  body.style.color = 'white'; 
+}
+
+function switchWhite() {
+  var body = document.querySelector('body');
+  body.style.backgroundColor = '#faf9f7'; 
+  body.style.color = 'black'; 
+}
